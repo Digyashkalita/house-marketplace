3fdb7d432392e4830e764df27d3f428748128712
Digyash kalita <www.digyash.kalita1@gmail.com>
2022-02-07 22:37:23 +0530

    firebase setup done

---
+++ src/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyCqylqF7brhwHgzkn06UGDZo0uWpd9ZpgI",
+  authDomain: "house-marketplace-app-a746e.firebaseapp.com",
+  projectId: "house-marketplace-app-a746e",
+  storageBucket: "house-marketplace-app-a746e.appspot.com",
+  messagingSenderId: "976557948995",
+  appId: "1:976557948995:web:e8ba57829d87a54ba854f6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
