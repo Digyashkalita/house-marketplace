ccd1348dfa4cb8d73586e3ed41ade06645c85cdb
Digyash kalita <www.digyash.kalita1@gmail.com>
2022-02-10 15:46:54 +0530

    forgot password functionality added

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState, useRef } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingstatus] = useState(true);
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    if (isMounted) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true);
+        }
+        setCheckingstatus(false);
+      });
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
+
+  return { loggedIn, checkingStatus };
+};
